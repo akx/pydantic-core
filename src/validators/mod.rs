2a65bb2b5f0385f113d4f9088e8acf25f1209985
David Montague <35119617+dmontagu@users.noreply.github.com>
2023-08-21 11:45:56 -0600

    Rework to use a single tuple schema that can express positional, variable, and variadic schemas

--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -484,8 +484,7 @@ pub fn build_validator<'a>(
         // decimals
         decimal::DecimalValidator,
         // tuples
-        tuple::TuplePositionalValidator,
-        tuple::TupleVariableValidator,
+        tuple::TupleValidator,
         // list/arrays
         list::ListValidator,
         // sets - unique lists
@@ -632,8 +631,7 @@ pub enum CombinedValidator {
     // sets - unique lists
     Set(set::SetValidator),
     // tuples
-    TuplePositional(tuple::TuplePositionalValidator),
-    TupleVariable(tuple::TupleVariableValidator),
+    Tuple(tuple::TupleValidator),
     // dicts/objects (recursive)
     Dict(dict::DictValidator),
     // None/null
